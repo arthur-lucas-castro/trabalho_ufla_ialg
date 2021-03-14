82f468e54d5dd83a517971d61e7a7b015634c22b
João Victor Oliveira <jvictor7.2001@gmail.com>
2021-03-13 22:29:47 -0300

    criacao do switch

--- Trabalho.cpp
+++ Trabalho.cpp
@@ -5,6 +5,8 @@
 #include <fstream>  
 using namespace std;
 
+void criaArquivo();
+
 struct Time
 {
 	string nome;
@@ -13,6 +15,36 @@ struct Time
 	int derrotas;
 	int empates;
 };
+
+int main()
+{
+	criaArquivo();
+	int escolhaDoQueFazer;
+	cout << "Bem vindo à lista de times do Brasileirão 2021! O quê você deseja fazer? Insira o número correspondente à sua escolha." << endl;
+	cout << "1. Inserir um time" << endl;
+	cout << "2. Ver a tabela do Brasileirão" << endl;
+	cout << "3. Editar as informações de algum time" << endl;
+	cout << "4. Deletar algum time" << endl;
+	cin >> escolhaDoQueFazer;
+	switch (escolhaDoQueFazer) {
+		case(1):
+			cout << "Funcionalidade indisponível no momento";
+			break;
+		case(2):
+			cout << "Funcionalidade indisponível no momento";
+			break;
+		case(3):
+			cout << "Funcionalidade indisponível no momento";
+			break;
+		case(4):
+			cout << "Funcionalidade indisponível no momento";
+			break;
+		default:
+			cout << "Escolha inválida";
+			break;
+	}
+
+}
 
 void criaArquivo() {
 	struct stat buffer;
@@ -25,9 +57,5 @@ void criaArquivo() {
 		outfile << "listona cabulosa" << endl;
 		outfile.close();
 	}
-}
-int main()
-{
-	criaArquivo();
 }
 
